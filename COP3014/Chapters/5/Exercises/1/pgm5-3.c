62b8de373d178e0c87294642c3f4b30cb8b4b37c
ShooShoSha <shooshosha@gmail.com>
2014-04-14 10:24:17 -0500

    Answer exerise.

---
+++ COP3014/Chapters/5/Exercises/1/pgm5-3.c
@@ -0,0 +1,20 @@
+// Program to generate a table of triangular numbers
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n, triangularNumber;
+
+	printf("Table of Triangular Numbers\n\n");
+	printf(" n  Sum from 1 to n\n");
+	printf("--- ---------------\n");
+
+	for(triangularNumber = 0, n = 1; n <= 10; ++n)
+	{
+		triangularNumber += n;
+		printf("%3i %15i\n", n, triangularNumber);
+	}
+
+	return 0;
+}
