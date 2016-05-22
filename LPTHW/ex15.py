ef4a5801670a53635a4c82c6dcc54ad6c474e618
Kevin O'Brien <shooshosha@gmail.com>
2016-05-21 21:12:32 -0700

    Complete study drills.

--- LPTHW/ex15.py
+++ LPTHW/ex15.py
@@ -1,18 +1,12 @@
+# Imports argv from sys module for command line parsing
 from sys import argv
 
+# Reads name of the script file and another string for the filename
 script, filename = argv
 
+# Open the specified filename as a file object
 txt = open(filename)
 
 print "Here's your file %r:" % filename
 print txt.read()
-txt.close()
-
-print "Type the filename again:"
-file_again = raw_input("> ")
-
-txt_again = open(file_again)
-
-print txt_again.read()
-txt_again.close()
 
