b38ff39235f37a8551c057cd6954c53138b39e34
Kevin O'Brien <shooshosha@gmail.com>
2016-05-27 12:10:00 -0700

    Complete study drills.

--- LPTHW/ex33.py
+++ LPTHW/ex33.py
@@ -1,16 +1,18 @@
-i = 0
-numbers = []
+def get_numbers(value, increment = 1):
+	i = 0 
+	numbers = []
 
-while i < 6:
-    print "At the top is %d" % i
-    numbers.append(i)
+	while i < value:
+		print "At the top is %d" % i
+		numbers.append(i)
 
-    i += 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+		i += increment
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
+	return numbers
 
 print "The numbers: "
 
-for number in numbers:
+for number in get_numbers(6,2):
     print number
 
